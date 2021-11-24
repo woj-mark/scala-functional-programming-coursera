1d8857cfd6ecd5df177616232827fd5ba36a9a3f
unknown <wtm.markowski@gmail.com>
2021-11-24 08:31:30 +0000

    First 4 methods added

--- funsets/funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/funsets/src/main/scala/funsets/FunSets.scala
@@ -18,32 +18,31 @@ trait FunSets extends FunSetsInterface:
   /**
    * Returns the set of the one given element.
    */
-  def singletonSet(elem: Int): FunSet = ???
+  def singletonSet(elem: Int): FunSet = (a:Int) => a == elem
 
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-  def union(s: FunSet, t: FunSet): FunSet = ???
+  def union(s: FunSet, t: FunSet): FunSet = (a:Int) => s(a) || t(a)
 
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-  def intersect(s: FunSet, t: FunSet): FunSet = ???
+  def intersect(s: FunSet, t: FunSet): FunSet = (a:Int) => s(a) && t(a)
 
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-  def diff(s: FunSet, t: FunSet): FunSet = ???
+  def diff(s: FunSet, t: FunSet): FunSet = (a:Int) => s(a) && !t(a)
 
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-  def filter(s: FunSet, p: Int => Boolean): FunSet = ???
-
+  def filter(s: FunSet, p: Int => Boolean): FunSet = 
 
   /**
    * The bounds for `forall` and `exists` are +/- 1000.
